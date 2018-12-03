5395aeaed956733956e0120ac72397c45f8d4c85
AlexFenfgCiso <alfeng@cisco.com>
2018-12-03 15:23:57 +0800

    add dncm_rest_api sample

--- mds_zone_active_compare_sample.py
+++ mds_zone_active_compare_sample.py
@@ -3,23 +3,24 @@ Created on Nov 20, 2018
 
 @author: alfeng
 '''
+import time
 import mds_tools as MT
 import mds_sh_run_files as SF
-import time
 
-start_time=time.time()    
 
-tool=MT.MdsTools()
+start_time = time.time()
 
-result = tool.compare_zone_active(SF.MDS_SH_RUN_01,SF.MDS_SH_RUN_02)
+tool = MT.MdsTools()
+
+result = tool.compare_zone_active(SF.MDS_SH_RUN_01, SF.MDS_SH_RUN_02)
 
 for item in result:
     print "error zone name "+item[0]
     print "sw01 error zone member" + item[1]
     print "sw01 error zone member" + item[2]
-    print ('='*200)
+    print '='*200
     
-end_time=time.time()-start_time    
+end_time = time.time()-start_time
 print "process during "+str(end_time)+"seconds"
 
 
