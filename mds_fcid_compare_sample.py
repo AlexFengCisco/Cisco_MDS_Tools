4b0359e874915da9b1c03a9655d38ce729f7e7ec
AlexFenfgCiso <alfeng@cisco.com>
2018-12-03 15:26:03 +0800

    add dncm_rest_api sample

--- mds_fcid_compare_sample.py
+++ mds_fcid_compare_sample.py
@@ -3,32 +3,28 @@ Created on Nov 20, 2018
 
 @author: alfeng
 '''
+import time
 import mds_tools as MT
 import mds_sh_run_files as SF
-import time
-
-start_time=time.time()    
-
-tool=MT.MdsTools()
-result= tool.compare_fcid_database(SF.MDS_SH_RUN_01,SF.MDS_SH_RUN_02)
 
 
+start_time = time.time()
 
-
-print "Switch MDS_01 fcid count is " +str(result[0])
-print "missed fcid list below "
-for item in result[3]:
-    print item
-
-print "Switch MDS_02 fcid count is " + str(result[2])
-print "missed fcid list below "
-for item in result[1]:
-    print item
-
-
-end_time = time.time()-start_time
-print "process during "+str(end_time)+"seconds"
+tool = MT.MdsTools()
+result = tool.compare_fcid_database(SF.MDS_SH_RUN_01, SF.MDS_SH_RUN_02)
 
 
 if __name__ == '__main__':
-    pass
+
+    print "Switch MDS_01 fcid count is " + str(result[0])
+    print "missed fcid list below "
+    for item in result[3]:
+        print item
+
+    print "Switch MDS_02 fcid count is " + str(result[2])
+    print "missed fcid list below "
+    for item in result[1]:
+        print item
+
+    end_time = time.time() - start_time
+    print "process during " + str(end_time) + "seconds"
--- mds_zone_active_compare_sample.py
+++ mds_zone_active_compare_sample.py
@@ -7,23 +7,18 @@ import time
 import mds_tools as MT
 import mds_sh_run_files as SF
 
-
-start_time = time.time()
-
-tool = MT.MdsTools()
-
-result = tool.compare_zone_active(SF.MDS_SH_RUN_01, SF.MDS_SH_RUN_02)
-
-for item in result:
-    print "error zone name "+item[0]
-    print "sw01 error zone member" + item[1]
-    print "sw01 error zone member" + item[2]
-    print '='*200
-    
-end_time = time.time()-start_time
-print "process during "+str(end_time)+"seconds"
-
-
-
 if __name__ == '__main__':
-    pass
+    start_time = time.time()
+
+    tool = MT.MdsTools()
+
+    result = tool.compare_zone_active(SF.MDS_SH_RUN_01, SF.MDS_SH_RUN_02)
+
+    for item in result:
+        print "error zone name " + item[0]
+        print "sw01 error zone member" + item[1]
+        print "sw01 error zone member" + item[2]
+        print '=' * 200
+
+    end_time = time.time() - start_time
+    print "process during " + str(end_time) + "seconds"
